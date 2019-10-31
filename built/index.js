c214d3f3d80746695a37f8eefa0317df13f8b577
鸡毛 <shihao.gu@perfma.com>
2019-10-31 11:33:44 +0800

    built

--- built/index.js
+++ built/index.js
@@ -88,7 +88,7 @@ function initMap(num) {
                             // 灌木道具
                             prop: Random(20) ? {
                                 resId: 0,
-                                resNum: 10,
+                                resNum: 11,
                                 passMask: 0,
                             } : undefined,
                         });
